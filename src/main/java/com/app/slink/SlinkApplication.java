c30db00fa2d3f8388490e3ef8d470110c4029e9b
john lester escarlan <jlescarlan11@gmail.com>
2025-08-21 17:37:06 +0800

    Initial commit

---
+++ src/main/java/com/app/slink/SlinkApplication.java
@@ -0,0 +1,13 @@
+package com.app.slink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/slink/SlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.slink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
